d5b00930052e68deba1c1097d0f10a600e7fb980
Jhonny91360 <jhonnyzamsa@hotmail.com>
2024-08-26 22:25:43 -0500

    [Add] theme configuration

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text>CalculatorScreen</Text>
+    <View style={globalStyles.calculatorContainer}>
+      <Text style={globalStyles.mainResult}>1500</Text>
+      <Text style={globalStyles.subResult}>225</Text>
     </View>
   );
 };
